f70f8cfd27ee755a0b35864470b8bf8a143c4f43
Ryan <ryan@ryanjm.com>
2013-10-22 21:06:18 -0600

    Added basic RDoc for all the schedule methods.
    This is in help of cleaning up the code. #3

--- lib/event_manager/schedule.rb
+++ lib/event_manager/schedule.rb
@@ -1,29 +1,52 @@
 require 'pry'
-# Encoding is heavely based off of ICS format
-# http://www.ietf.org/rfc/rfc2445.txt
-# 4.3.10 Recurrence Rule
-# It is HIGHLY suggested to read through the entire section before reading this code. By sticking closely to the ICS format, we should be able to easily expand out the options if we want to handle more cases.
-#
-# duration - ics assumes start/end dates to figure out how long an event should be and how to repeat it. I'm changing it a little by having a duration in days. My approach is for the schedule to define when something is DUE, and then duration should be how many days the inspector has to do it. I think the due date is more important then when it should be started.
+=begin rdoc
+
+This handles all of the logic right now.
+
+Encoding is heavely based off of ICS format
+http://www.ietf.org/rfc/rfc2445.txt
+4.3.10 Recurrence Rule
+It is HIGHLY suggested to read through the entire section before reading this code. By sticking closely to the ICS format, we should be able to easily expand out the options if we want to handle more cases.
+
+TO-DOs:
+
+- Should probably use better naming conventions. Using +encode+ and +decode+ would be a good starter.
+- Better names for variables, such as start_date. Though I do want to stick with ICS where appropriate.
+
+== Attributes
+
+[freq]          identifies type of recurrance. i.e. :daily, :weekly, :monthly (required)
+[interval]      how often to repeat (positive value, default = 1). Interval of 2 would be "every other [freq]"
+[by_day]        list of days, with possible value in front. (see ics BYDAY). 
+                  i.e. [MO,TU] would be Monday and Tuesday. [1MO,2TU] would be first monday (of the month) 
+                  and 2nd Tuesday (of the month).
+[by_month_day]  integer representing day in month, postive or negative. 
+                  i.e. [4] would be the 4th of the month. [-2,-4] would be the 2nd to last day and 4th to last day.
+[wkst]          defines when the week starts (defaults to Monday) - can't currently change
+[duration]      breaking from ics a little here - ics assumes start/end dates to figure out how long 
+                  an event should be and how to repeat it. I'm changing it a little by having a 
+                  duration in _days_. My approach is for the schedule to define when something is 
+                  DUE, and then duration should be how many days the event is. 
+[start_date]    when this schedule should go into effect.
  
+=end
+
 Event = Struct.new(:start_date, :end_date) do
 end
 
 class Schedule
 
-  attr_accessor :id
-  attr_accessor :freq # identifies type of recurrance (required)
-  attr_accessor :interval # how often to repeat (positive value, default = 1)
-  attr_accessor :by_day # list of days, with possible value in front
-  attr_accessor :by_month_day # integer representing day in month
-  # attr_accessor :by_week_no # integer representing week in year
-  # attr_accessor :by_month # integer representing mo in year
-  attr_accessor :wkst # defines when the week starts (defaults to Monday) - can't currently change
-  attr_accessor :duration # breaking from ics a little here (see above)
+  attr_accessor :freq 
+  attr_accessor :interval
+  attr_accessor :by_day
+  attr_accessor :by_month_day
+  attr_accessor :wkst
+  attr_accessor :duration
 
-  attr_accessor :start_date # when this should go into effect
+  attr_accessor :start_date
 
-  # set the defaults
+  ##
+  # set the default values
   def initialize
     @interval = 1
     @wkst = :mo
@@ -31,9 +54,27 @@ class Schedule
     @duration = 0 # 1 day
   end
 
+  # The list of frequencies currently supported
   FREQ = [:weekly, :monthly]
+
+  # List of days of week
   DAYS = [:su, :mo, :tu, :we, :th, :fr, :sa]
 
+  ##
+  # Encodes the days_of_week so that it can be saved to database.
+  #
+  # == Attributes
+  #
+  # [+days_of_week+]  This is an array of strings with two letter representaiton of the day.
+  #                     i.e. ["Mo", "We", "Fr"]
+  # [+offset+]        String with the number of offset. According to ICS each day could have
+  #                     it's own offset, but for now, every day will get the same one.
+  #                     i.e. '2'
+  #
+  # == Return
+  #
+  # Returns a string format of the array.
+  #   i.e. '2mo,2we'
   def convert_by_day(days_of_week, offset = '')
     selected_days = days_of_week.map do |day|
       d = day.downcase.to_sym
@@ -49,10 +90,21 @@ class Schedule
     selected_days.compact.join(",")
   end
 
-  # Returns the date that satisfies the offset and wday
-  # used for "first Monday of the month" or "last Monday of the month"
-  # offset has to be > 0
-  # TODO: isn't dependent on Schedule. Might be better as apart of the date object
+  ##
+  # Used for "first Monday of the month" or "last Monday of the month"
+  #
+  # Offset has to be > 0
+  #
+  # == Attributes
+  #
+  # [year]    Year in which to find the valid date
+  # [month]   Month in which to find the valid date
+  # [offset]  What offset to find. i.e 2 would be "second [wday] of month"
+  # [wday]    Which day of the week to find.
+  #
+  # == Return
+  #
+  # Returns a day that satisfies the offset and wday.
   def day_of_month(year,month,offset,wday)
     first = Date.new(year,month) # first day of month
     last = Date.new(year,month).next_month - 1 # grab the last day
@@ -87,13 +139,12 @@ class Schedule
     end
   end
 
+  ##
   # Take params from a form and build the needed attributes
-  # Possible attributes:
-  # freq - type of schedule, "weekly" or "monthly"
-  # interval - how often the frequency should repeat
-  # days_of_week - array of days the schedule gets applys to (ie ["Mo","We"])
-  # days_of_week_offset - how to offset the days of the week, just a single number
-  # duration - how long someone has to complete the task (0 = due same day)
+  #
+  # == Attributes
+  #
+  # See list of attributes above
   def create(params)
 
     if (params[:freq] && FREQ.include?(params[:freq].to_sym))
@@ -116,9 +167,14 @@ class Schedule
     @start_date = params[:start_date]
   end
   
-  # Check to see if it is valid
+  ##
+  # Check to see if the schedule is valid
+  #
+  # Right now this is a pretty simple validation. Just 
+  # need to make sure the +@freq+ is set and that the
+  # +@duration+ is longer than the frequency_length
   def valid?
-    if @name.nil? || @freq.nil?
+    if @freq.nil?
       false
     elsif frequency_length < @duration
       false
@@ -127,7 +183,11 @@ class Schedule
     end
   end
 
-  # simply used for validation - we'll keep the monthly simple for now
+  ##
+  # This is just used for the validation method.
+  #
+  # This is a pretty simplistic view. Might not be robust 
+  # enough going forward. Especially monthly.
   def frequency_length
     if @freq == :daily
       @interval * 1
@@ -138,9 +198,18 @@ class Schedule
     end
   end
 
-  # Converts @by_day to an nested array
-  # The first part is the offset (default = 1) and second is wday number
-  # offset would be used for things like "second Monday of the month" ([2,1])
+  ##
+  # This decodes +@by_day+ into a nested array.
+  #
+  # == Return
+  #
+  # It returns a nested array. The outside array is for each day. 
+  # The inside array always has two elements. The first is the offset,
+  # the second is the day of the week number (wday).
+  # An example would be the "second Monday of the month" which would 
+  # look like: [2,1]
+  #  
+  # Examples:
   # 'mo' => [[1,1]]
   # 'mo,we,fr' => [[1,1],[1,3],[1,5]]
   def translate_by_day
@@ -156,7 +225,18 @@ class Schedule
     end
   end
 
-  # for :weekly - returns index of first day in translate_by_day
+  ##
+  # Used for the +:weekly+ and +:monthly+ frequencies. This 
+  # takes the output of +translate_by_day+, sorts it, and then
+  # returns the index of the first day in the group.
+  #
+  # This normally shouldn't _need_ to do anything. They should 
+  # be in order, but that can't be gurenteed coming from the form.
+  # Maybe instead this should be sorted before saving to the db.
+  #
+  # == Return
+  #
+  # Returns the index of the first day in +translate_by_day+
   def first_day
     if @freq == :weekly || (@freq == :monthly && @by_day)
       days = translate_by_day
@@ -165,10 +245,25 @@ class Schedule
     end
   end
 
-  # Will return the _first_ time this event should happen after a given date
-  # `continue` option is if it should look into the following freq or not (ie look at the next week).
-  #     Otherwise it returns nil.
-  # Does not take into account the interval.
+  ##
+  # Used to find the next time an event should happen aften a given date.
+  #
+  # This does not take into account the interval.
+  #
+  # This is the most complicated method since it does deal with the logic 
+  # for each kind of frequency.
+  #
+  # == Attributes
+  #
+  # [+start_date+]  What date to search after.
+  # [+continue+]    This option is if it should look into the following 
+  #                   freq or not (i.ee look at the next week). 
+  #
+  # == Return
+  #
+  # It will return the date of the next time an event will happen within 
+  # the frequency. If it doesn't find one it will return +nil+, unless 
+  # +continue+ is true, in which case it will go to the next frequency.
   def next_occurrence(start_date, continue=false)
     if @freq == :weekly
       wday = start_date.wday
@@ -238,18 +333,48 @@ class Schedule
     end
   end
 
-  # Hack - this is from ActiveSupport - just use that method
+  # This is a hack - this is from ActiveSupport, when we integrate with Rails we should just use that method
   COMMON_YEAR_DAYS_IN_MONTH = [nil, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
+  ##
+  # Returns how many days are in the month.
+  #
+  # This is a Rails method, so I won't add documentation for it 
+  # (this should be removed later).
   def days_in_month(month, year = Time.now.year)
      return 29 if month == 2 && Date.gregorian_leap?(year)
      COMMON_YEAR_DAYS_IN_MONTH[month]
   end
 
-  # Returns the first day for the frequency
+  ##
+  # Returns the first day for the frequency. Might not be the first occurrence.
+  #
+  # TODO: Looks like this is only being used for +:weekly+. I think this
+  # ended up with the multiple +elseif+ within +next_occurrence+ due to 
+  # edge cases. Totally up for restructuring the logic for these methods.
+  #
+  # TODO: looks like there might be an edge case that isn't being handled.
+  # This could return a date that is _before_ your +start_date+. That is if 
+  # it is MWF and you give it Saturday, it will give you Monday of that week.
+  # Therefore when you use this method, you should have to check to see
+  # if it is infact greater than +start_date+, if not, then find the next 
+  # occurance of it. I think.
+  #
+  # == Attributes
+  #
+  # [start_date]  Date within frequency which to find the first occurrance.
+  #
+  # == Return
+  #
+  # Returns a date which is the correct weekday (wday) next to +start_date+.
   def first_group(start_date)
     if @freq == :weekly
+      # Grab the first wday within the schedule
       wday = translate_by_day[first_day][1]
+      # Based off the start date add the number of days it takes to get to 
+      # the correct week day.
+      # i.e. start_date is Friday (5) and wday is Monday(1)
+      # start_date + (1 - 5) = Friday - 4 = Monday of that week.
       start_date + (wday - start_date.wday)
     # elsif @freq == :monthly
     #   day = translate_by_day[first_day]
@@ -257,7 +382,18 @@ class Schedule
     end
   end
 
+  ##
   # Returns the first date, for a grouping of events
+  #
+  # == Attributes
+  #
+  # [+first_occurrence+]  The first time this schedule _does_ happen.
+  # [+after_date+]        The date which you want the group to happen _after_.
+  #
+  # == Return
+  #
+  # Date of the first day in the group that happens after the search date.
+  #
   # i.e. if a schedule is MWF, then it will return the Monday
   # if it is every other week then it will return the following Monday that matches
   def next_group(first_occurrence,after_date)
@@ -283,8 +419,18 @@ class Schedule
     first_occurrence + days_after
   end
 
-  # Finds the next occurance of the schedule as long as it is between the two dates
-  # TODO: Possible refactoring, pass in first_occurance, not start_date
+  ##
+  # Finds the next occurance of the schedule as long as it is between the two dates.
+  #
+  # This is invisioned to be a public method.
+  # 
+  # == Attributes
+  #
+  # [+after_date+]  Date which to search after.
+  #
+  # == Return
+  #
+  # Returns a date of when the next event happens after the search date.
   def next_date(after_date)
     # puts "\\nnext_date - start(#{start_date}) after(#{after_date})"
     first_occurrence = next_occurrence(start_date,true)
@@ -307,7 +453,19 @@ class Schedule
     end
   end
 
-  # be able to grab the next occurances after (and including) date_start and before date_end
+  ##
+  # Get a list of events that happen between two dates.
+  #
+  # == Attributes
+  #
+  # [+date_start+]  Date on which you want the search to start.
+  # [+date_end+]    Date when you want the search to stop.
+  #
+  # == Return
+  #
+  # Returns an array of +Event+ structs each having +start_date+ 
+  # and +end_date+. These events will happen on or after the +date_start+
+  # and before +date_end+.
   def events_between(date_start, date_end)
 
     events = []
